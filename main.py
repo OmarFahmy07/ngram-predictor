011ecb6fd8f6a8df9a21eb6037df64a79dcad8d3
Omar Fahmy <omar.fahmy@analog.com>
2026-04-08 19:11:36 +0200

    M1: adding Normalizer into dataprep pipeline

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import argparse
+import os
+
+import nltk
 from dotenv import load_dotenv
 
+from src.data_prep.normalizer import Normalizer
+
+
 def main():
-    # Read configuration from .env file
+    
     load_dotenv("config/.env")
 
-    # Set up command-line argument parsing
     parser = argparse.ArgumentParser(
-        description="N-Gram Next-Word Predictor (Capstone Project)")
-    
-    # Define the command-line argument for selecting the pipeline step
+        description="N-Gram Next-Word Predictor (Capstone Project)"
+    )
     parser.add_argument(
         "--step",
         choices=["dataprep", "model", "inference", "all"],
-        help="Which pipeline step to run.")
-    
-    # Parse the command-line arguments
-    parser.parse_args()
+        help="Which pipeline step to run."
+    )
 
-# This is a guard to ensure that the main function is called only when this 
-# script is executed directly, and not when imported as a module.
+    args = parser.parse_args()
+
+    if args.step == "dataprep":
+        
+        nltk.download("punkt_tab", quiet=True)
+
+        normalizer = Normalizer()
+
+        train_raw_dir = os.getenv("TRAIN_RAW_DIR")
+        train_tokens_path = os.getenv("TRAIN_TOKENS")
+
+        text = normalizer.load(train_raw_dir)
+
+        text = normalizer.strip_gutenberg(text)
+
+        text = normalizer.lowercase(text)
+
+        sentences = normalizer.sentence_tokenize(text)
+
+        sentences = sentences[:100]
+
+        tokenized_sentences = []
+        for sentence in sentences:
+            sentence = normalizer.remove_punctuation(sentence)
+            sentence = normalizer.remove_numbers(sentence)
+            sentence = normalizer.remove_whitespace(sentence)
+
+            tokens = normalizer.word_tokenize(sentence)
+            tokenized_sentences.append(tokens)
+
+        normalizer.save(tokenized_sentences, train_tokens_path)
+
+
 if __name__ == "__main__":
     main()
